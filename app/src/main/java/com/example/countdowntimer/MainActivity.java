a1a0d40508479b58f3ccce06ca9e26cea5e2f8af
Quockm <mjnhquoc21101997@gmail.com>
2019-12-19 19:20:00 +0700

    ViewFlipper

--- app/src/main/java/com/example/countdowntimer/MainActivity.java
+++ app/src/main/java/com/example/countdowntimer/MainActivity.java
@@ -8,6 +8,8 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.RelativeLayout;
+import android.widget.ViewFlipper;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -18,42 +20,76 @@ public class MainActivity extends AppCompatActivity {
             R.drawable.image4,
 
     };
-    ImageView mImgBanner;
+    ViewFlipper mViewFlipper;
     Button mBtnStart;
-    int mcount =0;
+    Button mBtnStop;
     boolean mIsloading = false;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+
         mBtnStart = findViewById(R.id.ButtonStart);
-        mImgBanner = findViewById(R.id.imageviewBanner);
+        mBtnStop = findViewById(R.id.ButtonStop);
+        mViewFlipper = findViewById(R.id.viewFlipper);
+
+        for (int i=0;i<mArrayImages.length;i++)
+        {
+            ImageView imageView = new ImageView(this);
+            imageView.setImageResource(mArrayImages[i]);
+            imageView.setScaleType(ImageView.ScaleType.FIT_XY);
+            mViewFlipper.addView(imageView);
+        }
 
         mBtnStart.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                if (mIsloading) {
+                if (mViewFlipper.isFlipping()){
                     return;
-                } else
-                    mIsloading = true; {
-                    CountDownTimer countDownTimer = new CountDownTimer((mArrayImages.length * 1000)+1000, 1000) {
-                        @Override
-                        public void onTick(long millisUnitFinished) {
-                            if (mcount >=mArrayImages.length) {
-                                mcount=0;
-                            }
-                            mImgBanner.setImageResource(mArrayImages[mcount++]);
-                        }
-                        @Override
-                        public void onFinish() {
-                                mcount =0;
-                                this.start();
-                        }
-                    };
-                    countDownTimer.start();
                 }
+                mViewFlipper.setFlipInterval(500);
+                mViewFlipper.startFlipping();
+            };
+        });
+
+        mBtnStop.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mViewFlipper.stopFlipping();
+
             }
         });
+
+//        mBtnStart = findViewById(R.id.ButtonStart);
+//        mImgBanner = findViewById(R.id.imageviewBanner);
+//
+//        mBtnStart.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                if (mIsloading) {
+//                    return;
+//                } else
+//                    mIsloading = true; {
+//                    CountDownTimer countDownTimer = new CountDownTimer((mArrayImages.length * 1000)+1000, 1000) {
+//                        @Override
+//                        public void onTick(long millisUnitFinished) {
+//                            if (mcount >=mArrayImages.length) {
+//                                mcount=0;
+//                            }
+//                            mImgBanner.setImageResource(mArrayImages[mcount++]);
+//                        }
+//                        @Override
+//                        public void onFinish() {
+//                                mcount =0;
+//                                this.start();
+//                        }
+//                    };
+//                    countDownTimer.start();
+//                }
+//            }
+//        });
+
+
     }
 }
